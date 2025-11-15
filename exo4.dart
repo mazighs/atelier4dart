c805d4a303db23bf18a9a51168c4e2ec058483f2
mazighs <barachemazighoo@.com>
2025-11-14 21:45:18 -0500

    ajouter une methode

--- exo4.dart
+++ exo4.dart
@@ -1,18 +1,8 @@
 class Media {
   final String titre;
-
-  Media(this.titre);
-    void afficherType() {
-    print("Ceci est un média générique.");
-  }
-}
-
-class Livre extends Media {
-  final String auteur;
-
-  Livre(String titre, this.auteur) : super(titre);
-  @override
+   Media(this.titre);
+   
   void afficherType() {
-    print("Ceci est un Livre : $titre par $auteur.");
+    print("Ceci est un média générique.");
   }
 }
