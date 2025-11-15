703cd1b3f5421d7e0eb6d97114a8accf093d1fd9
mazighs <barachemazighoo@.com>
2025-11-14 21:36:05 -0500

    Redéfinissez la méthode afficherType

--- exo4.dart
+++ exo4.dart
@@ -6,3 +6,13 @@ class Media {
     print("Ceci est un média générique.");
   }
 }
+
+class Livre extends Media {
+  final String auteur;
+
+  Livre(String titre, this.auteur) : super(titre);
+  @override
+  void afficherType() {
+    print("Ceci est un Livre : $titre par $auteur.");
+  }
+}
