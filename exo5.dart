42429213435f44dfff55dd519f1450ddab8aa1c8
mazighs <barachemazighoo@.com>
2025-11-14 22:13:45 -0500

    Implémentez obligatoirement la méthode calculerAire() pour retourner l'aire du cercle

---
+++ exo5.dart
@@ -0,0 +1,18 @@
+abstract class Forme {
+  double calculerAire();
+    void afficherMessage() {
+    print("Calcul de l'aire...");
+  }
+}
+
+class Cercle extends Forme {
+  final double rayon;
+
+  Cercle(this.rayon);
+
+  @override
+  double calculerAire() {
+    return 3.1416 * rayon * rayon;
+  }
+}
+
