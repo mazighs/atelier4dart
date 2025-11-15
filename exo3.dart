c79b152c75202439200f2f2bf9ca0f5cef89eed1
mazighs <barachemazighoo@.com>
2025-11-14 21:21:08 -0500

    Implémentez une méthode publique depot(double montant) qui ajoute le montant à _solde seulement si montant est positif

---
+++ exo3.dart
@@ -0,0 +1,10 @@
+class Compte {
+  double _solde = 0.0;
+  final String numeroCompte;
+   Compte(this.numeroCompte, [double soldeInitial = 0.0]) {
+    _solde = soldeInitial;
+  }
+   
+    }
+  }
+}
