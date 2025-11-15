5030d4e385db9c75939d23b51fbe78ef87b2f72c
mazighs <barachemazighoo@.com>
2025-11-14 23:37:22 -0500

    Dans main(), créez plusieurs objets de Livre et afficher leurs informations

--- exo8.dart
+++ exo8.dart
@@ -7,33 +7,9 @@ class Livre {
   Livre(this.titre, this.auteur) {
     totalLivres++;
   }
-  
-  int get pages => _pages;
+    int get pages => _pages;
 
   void afficherInfos() {
     print("Titre: $titre, Auteur: $auteur, Pages: $pages");
   }
-
-  static void afficherTotal() {
-    print("Total de livres créés : $totalLivres");
-  }
-}
-
-class Roman extends Livre {
-  String genre;
-
-  Roman(String titre, String auteur, this.genre)
-      : super(titre, auteur);
-
-  void afficherRoman() {
-    print("Titre: $titre, Auteur: $auteur, Genre: $genre");
-  }
-}
-
-void main() {
-  var l1 = Livre("Sapiens", "Yuval Noah Harari");
-  var l2 = Livre("Clean Code", "Robert C. Martin");
-
-  l1.afficherInfos();
-  l2.afficherInfos();
-  
+ 
